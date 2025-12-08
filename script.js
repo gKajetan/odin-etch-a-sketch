1537c6ac0737c2583e9b27302c1f35937e23c0d6
Kajetan <147710182+gKajetan@users.noreply.github.com>
2025-12-08 17:08:55 +0100

    Commented grid code

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ const container = document.querySelector('.squaresContainer');
 
 let size = 4;
 
+// create rows
 for (let i = 0; i < size; i++) {
     let row = document.createElement('div');
     row.classList.add('row');
 
+    // create squares INSIDE rows
     for (let j = 0; j < size; j++) {
         let square = document.createElement('div');
         square.classList.add('square'); 
         row.appendChild(square); 
     }
-    
     container.appendChild(row);
 }

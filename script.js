fcfc4649695e6435ec3edf9152e8384283b567a1
Kajetan <147710182+gKajetan@users.noreply.github.com>
2025-12-08 17:00:51 +0100

    Squares/grid generation

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('init');
+const container = document.querySelector('.squaresContainer');
+
+let size = 4;
+
+for (let i = 0; i < size; i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+
+    for (let j = 0; j < size; j++) {
+        let square = document.createElement('div');
+        square.classList.add('square'); 
+        row.appendChild(square); 
+    }
+    
+    container.appendChild(row);
+}
